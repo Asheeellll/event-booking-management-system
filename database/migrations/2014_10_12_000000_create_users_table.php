045d36d9364ef24c7d98b92d7bd2e127216b77d3
Asheel Bava <myselfasheel@gmail.com>
2026-07-29 00:49:19 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,10 +12,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
+
+            // Required by your project
+            $table->string('phone')->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user');
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
             $table->timestamps();
         });
