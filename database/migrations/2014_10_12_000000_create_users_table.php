bbd3e364dfced50b86b17472cbd4d5545b0a0dce
Asheel Bava <myselfasheel@gmail.com>
2026-07-28 22:41:15 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    public bool $withinTransaction = false
     /**
      * Run the migrations.
      * This creates the 'users' table in the database.
